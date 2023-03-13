e6719aeda52dc5eabd988c3dbc92299729fa1227
GuilhermePie <guilhermeSpiedade@gmail.com>
2023-03-13 18:15:58 -0300

    Update filas.js

--- js/filas.js
+++ js/filas.js
@@ -1,33 +1,35 @@
-function Queue(){
-    var itens = []
+//-----------------------------------------------------------------------------------------
+// function Queue(){
+//     var itens = []
 
-    this.enqueue = function(){
-        //adiciona um novo item
-        itens.push(element)
-    }
+//     this.enqueue = function(){
+//         //adiciona um novo item
+//         itens.push(element)
+//     }
 
-    this.dequeue = function(){
-        //remove um item
-        return itens.shift()
-    }
+//     this.dequeue = function(){
+//         //remove um item
+//         return itens.shift()
+//     }
 
-    this.front = function(){
-        //retorna o primeiro elemento da fila 
-        return itens[0]
-    }
+//     this.front = function(){
+//         //retorna o primeiro elemento da fila 
+//         return itens[0]
+//     }
 
-    this.isEmpty = function(){
-        //verifica se a fila está vazia
-        return itens.length === 0 //igual 0 = true / diferente de 0 = false
-    }
+//     this.isEmpty = function(){
+//         //verifica se a fila está vazia
+//         return itens.length === 0 //igual 0 = true / diferente de 0 = false
+//     }
 
-    this.size = function(){
-        //retorna o tamanho da fila
-        return itens.length
-    }
+//     this.size = function(){
+//         //retorna o tamanho da fila
+//         return itens.length
+//     }
 
-    this.print = function(){
-        //imprimir a fila no console
-        console.log(itens.toString())
-    }
-}
+//     this.print = function(){
+//         //imprimir a fila no console
+//         console.log(itens.toString())
+//     }
+// }
+//-----------------------------------------------------------------------------------------
